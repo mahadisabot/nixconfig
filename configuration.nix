ff7ed421cdab62e298dd1bf4d6038fa95abf2e0d
mahadisabot <79964283+mahadisabot@users.noreply.github.com>
2023-10-29 15:41:52 +0000

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,214 @@
+{ config, pkgs, inputs, ... }:
+
+{
+    # Remove unecessary preinstalled packages
+    environment.defaultPackages = [ ];
+    services.xserver.desktopManager.xterm.enable = false;
+services.xserver.enable = true;
+services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = false;
+    programs.zsh.enable = true;
+
+    # Laptop-specific packages (the other ones are installed in `packages.nix`)
+    environment.systemPackages = with pkgs; [
+        acpi tlp git
+alacritty
+starship
+neovim
+rofi-wayland
+dunst
+nitrogen
+lxappearance
+libsForQt5.breeze-qt5
+libsForQt5.breeze-gtk
+libsForQt5.dolphin
+exa
+xfce.tumbler
+cliphist
+python311Packages.python-pipedrive
+pavucontrol
+neofetch
+cmatrix
+btop
+networkmanager
+networkmanagerapplet
+firewalld-gui
+firewalld
+blueman
+bluez
+waybar
+bibata-cursors
+kora-icon-theme
+swww
+wlogout
+pfetch
+    ];
+
+    # Install fonts
+    fonts = {
+        fonts = with pkgs; [
+            ttf-font-awesome
+            JetBrainsMono
+            ttf-fira-sans
+            openmoji-color
+            ttf-firacode-nerd
+            ttf-fira-code
+            #(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        ];
+
+        fontconfig = {
+            hinting.autohint = true;
+            defaultFonts = {
+              emoji = [ "OpenMoji Color" ];
+            };
+        };
+    };
+
+services.blueman.enable = true;
+    # Wayland stuff: enable XDG integration, allow sway to use brillo
+    xdg = {
+        portal = {
+            enable = true;
+            extraPortals = with pkgs; [
+                xdg-desktop-portal-wlr
+                xdg-desktop-portal-gtk
+            ];
+            gtkUsePortal = true;
+        };
+    };
+
+    # Nix settings, auto cleanup and enable flakes
+    nix = {
+        settings.auto-optimise-store = true;
+        settings.allowed-users = [ "notus" ];
+        gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 7d";
+        };
+        extraOptions = ''
+            experimental-features = nix-command flakes
+            keep-outputs = true
+            keep-derivations = true
+        '';
+    };
+
+    # Boot settings: clean /tmp/, latest kernel and enable bootloader
+    boot = {
+        cleanTmpDir = true;
+        loader = {
+        systemd-boot.enable = true;
+        systemd-boot.editor = true;
+        efi.canTouchEfiVariables = true;
+        timeout = 3;
+        };
+    };
+
+    # Set up locales (timezone and keyboard layout)
+    time.timeZone = "Asia/Karachi";
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        keyMap = "us";
+    };
+
+    # Set up user and enable sudo
+    users.users.notus = {
+        isNormalUser = true;
+        extraGroups = [ "input" "wheel" ];
+        shell = pkgs.zsh;
+    };
+      users.users.pwn = {
+    isNormalUser = true;
+    description = "pwn";
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" "input"];
+ 
+    packages = with pkgs; [
+     
+      kate
+      zsh
+    #  thunderbird
+    ];
+  };
+
+    # Set up networking and secure it
+    networking = {
+        wireless.iwd.enable = true;
+        firewall = {
+            enable = true;
+            allowedTCPPorts = [ 443 80 ];
+            allowedUDPPorts = [ 443 80 44857 ];
+            allowPing = false;
+        };
+    };
+
+    # Set environment variables
+    environment.variables = {
+        NIXOS_CONFIG = "$HOME/.config/nixos/configuration.nix";
+        NIXOS_CONFIG_DIR = "$HOME/.config/nixos/";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+        GTK_RC_FILES = "$HOME/.local/share/gtk-1.0/gtkrc";
+        GTK2_RC_FILES = "$HOME/.local/share/gtk-2.0/gtkrc";
+        MOZ_ENABLE_WAYLAND = "1";
+        ZK_NOTEBOOK_DIR = "$HOME/stuff/notes/";
+        EDITOR = "nvim";
+        DIRENV_LOG_FORMAT = "";
+        ANKI_WAYLAND = "1";
+        DISABLE_QT5_COMPAT = "0";
+    };
+
+    # Security 
+    security = {
+        sudo.enable = true;
+        doas = {
+            enable = true;
+            extraRules = [{
+                users = [ "pwn" "notus" ];
+                keepEnv = true;
+                persist = true;
+            }];
+        };
+
+        # Extra security
+        protectKernelImage = true;
+    };
+
+    # Sound
+    sound = {
+        enable = true;
+    };
+
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
+    
+    # Disable bluetooth, enable pulseaudio, enable opengl (for Wayland)
+    hardware = {
+        bluetooth.enable = true;
+        opengl = {
+            enable = true;
+            driSupport = true;
+        };
+    };
+programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+
+    # Optional
+    # Whether to enable patching wlroots for better Nvidia support
+   # enableNvidiaPatches = true;
+  };
+
+    # Do not touch
+    system.stateVersion = "20.09";
+}
